232d26ecbfa69fcec7750149ece63ddd8c406f1b
bdoink <gutgik@outlook.com>
2026-06-16 16:44:05 +0700

    removed banners

--- src/data/banners.js
+++ src/data/banners.js
@@ -14,15 +14,6 @@ function createBanner(banner) {
 
 export const banners = [
 	{
-		title: 'Скидка <span class="text-[#FF7948]">20%</span><br>на чистку зубов',
-		text1:
-			'Здоровье зубов начинается с чистоты. <br class="hidden md:block" /> Специально для вас — скидка 20 % на профессиональную чистку.',
-		text2: 'Подарите себе заботу о улыбке по выгодной цене!',
-		mobileImage: './assets/Girl1-Photoroom.png?v=20260517',
-		desktopImage: './assets/Girl1-Photoroom2.png?v=20260517',
-		imageAlt: 'Баннер акции на чистку зубов',
-	},
-	{
 		title: 'Скидка <span class="text-[#FF7948]">20%</span><br>на 3D-томографию',
 		text1:
 			'Точная диагностика — первый шаг к здоровой улыбке. <br class="hidden md:block" />Запишитесь сейчас и получите доступ к точной диагностике.',
@@ -30,15 +21,6 @@ export const banners = [
 		mobileImage: './assets/Girl2-Photoroom.png?v=20260517',
 		desktopImage: './assets/Girl2-Photoroom2.png?v=20260517',
 		imageAlt: 'Баннер 3D-томографии',
-	},
-	{
-		title: 'Скидка <span class="text-[#FF7948]">20%</span><br>на ортодонтию',
-		text1:
-			'Мечтаете о ровной улыбке? Сейчас — самое время! <br class="hidden md:block" />Получите скидку 20 % на все ортодонтические услуги.',
-		text2: 'Начните путь к идеальным зубам уже сегодня!',
-		mobileImage: './assets/Girl3-Photoroom.png?v=20260517',
-		desktopImage: './assets/Girl3-Photoroom2.png?v=20260517',
-		imageAlt: 'Баннер ортодонтии',
 	},
 	{
 		title: 'Ищем сотрудников:',
